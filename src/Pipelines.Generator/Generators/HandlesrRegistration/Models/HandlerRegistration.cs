0632fb74e1950d196b7c375f3b3c74608be360bf
Panos Athanasiou <panos_ath@outlook.com>
2026-05-02 15:55:44 +0300

    refactor(HandlerRegistration): to check for Ignore attribute
    - set disabled to true if ignore attribute is found
    - if disabled then nothing is added to the source builder
    - implemented Equas and GetHashCode

--- src/Pipelines.Generator/Generators/HandlesrRegistration/Models/HandlerRegistration.cs
+++ src/Pipelines.Generator/Generators/HandlesrRegistration/Models/HandlerRegistration.cs
@@ -2,19 +2,36 @@
 
 internal readonly record struct HandlerRegistration
 {
-    public readonly string Handler;
-    public readonly string Interface;
-    public readonly SyntaxNode Node;
+    private readonly bool Disabled;
 
-    public HandlerRegistration(SyntaxNode node, INamedTypeSymbol classSymbol, INamedTypeSymbol interfaceSymbol)
+    public readonly string Handler;
+
+    public readonly string Interface;
+
+    public HandlerRegistration(INamedTypeSymbol classSymbol, INamedTypeSymbol interfaceSymbol)
     {
+        var attributes = classSymbol.GetAttributes();
+
+        Disabled = attributes.Any(x => x.IsIgnoreAttribute());
         Handler = classSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
         Interface = interfaceSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-        Node = node;
     }
 
     public void ServiceRegistration(SourceBuilder sb)
     {
+        if (Disabled) return;
+
         sb.Line(sb, $"services.AddScoped<{Interface}, {Handler}>();");
+    }
+
+    public bool Equals(HandlerRegistration other)
+    {
+        return other.Handler == Handler
+            && other.Interface == Interface;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Handler, Interface);
     }
 }
